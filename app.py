1081879e4e422b4c6a7716803d4daa65f69dd038
Pulkit <64334269+pulkitgupta1109@users.noreply.github.com>
2022-11-30 22:23:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+def welcome():
+    return "Welcome All"
+
+def main():
+  st.title("Subtraction")
+  html_temp = """
+  <div style="background-color:tomato;padding:10px">
+  <h2 style="color:white;text-align:center;">Subtraction of 2 numbers using Streamlit</h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  num = st.number_input("Enter number to check if it's even or odd")
+  if num//2 ==0:
+	result = "Even Number"
+  else:
+ 	result = "Odd Number" 
+  st.success('Given number {} is {}'.format(num, result))
+  if st.button("Made By"):
+      st.text("Pulkit")
+      st.text("21f1000879")
+
+if __name__=='__main__':
+  main()
